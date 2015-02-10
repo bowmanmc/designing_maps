833174c5824c9854c71fa819ff14a45709e21902
Michael Bowman <bowmanmc@gmail.com>
2015-02-09 20:17:30 -0500

    Basic map completed

--- app/scripts/basic.js
+++ app/scripts/basic.js
@@ -1,57 +1,64 @@
 
-function OhioMap(elementIdSelector) {
+function OhioMap(elementId) {
 
-    this.mapSelector = elementIdSelector;
+    this.divId = elementId;
+    this.divSelector = '#' + elementId;
 
-    this.drawMap = function() {
+    this.draw = function() {
 
         var map = this;
 
-        var el = $(map.mapSelector)[0];
-        if (typeof el === 'undefined') {
-            console.log('ERROR - No element matching: ' + this.mapSelector);
-            return;
-        }
+        var el = document.getElementById(map.divId);
 
-        //var width = 960,
-        //    height = 1200;
+        // Ohio Map Dimensions: 960x1200 (width x height)
+        // Use this to scale the map up/down depending on
+        // size of map container.
         map.height = el.clientHeight;
         map.width = (960 / 1200) * map.height;
         console.log('Making map size: ' + map.width + 'x' + map.height);
 
-        map.projection = ProjectionFactory.mercator(map.width, map.height);
-        //map.projection = ProjectionFactory.conicConformal(map.width, map.height);
-
+        // For more on map projections, see:
+        // https://github.com/mbostock/d3/wiki/Geo-Projections
+        map.projection = d3.geo.conicConformal();
         map.path = d3.geo.path().projection(map.projection);
 
-        map.svg = d3.select(map.mapSelector).append('svg')
+        map.svg = d3.select(map.divSelector).append('svg')
             .attr('width', map.width)
             .attr('height', map.height);
 
-        this.drawState();
+        map.getState().then(function(data) {
+            map.drawState(data);
+        });
     };
 
-    this.drawState = function() {
+    this.getState = function() {
+        var deferred = $.Deferred();
+        d3.json('maps/state.oh.json', function(error, response) {
+            deferred.resolve(response);
+        });
+        return deferred.promise();
+    };
+
+    this.drawState = function(data) {
 
         var map = this;
-        d3.json('maps/state.oh.json', function(error, response) {
 
-            map.projection.scale(1).translate([0, 0]);
+        var centroid = d3.geo.centroid(data.features[0]);
+        var r = [centroid[0] * -1, centroid[1] * -1];
+        // Start the projection from defaults (looking at Ohio)
+        map.projection.scale(1).translate([0, 0]).rotate(r);
 
-            var b = map.path.bounds(response),
-                s = 0.95 / Math.max((b[1][0] - b[0][0]) / map.width, (b[1][1] - b[0][1]) / map.height),
-                t = [(map.width - s * (b[1][0] + b[0][0])) / 2, (map.height - s * (b[1][1] + b[0][1])) / 2];
+        var b = map.path.bounds(data),
+            s = 0.95 / Math.max((b[1][0] - b[0][0]) / map.width, (b[1][1] - b[0][1]) / map.height),
+            t = [(map.width - s * (b[1][0] + b[0][0])) / 2, (map.height - s * (b[1][1] + b[0][1])) / 2];
 
-            map.projection.scale(s).translate(t);
+        map.projection.scale(s).translate(t);
 
-            map.svg.selectAll('path')
-                .data(response.features)
-                .enter().append('path')
-                .attr('class', 'state')
-                .attr('d', map.path);
-
-        });
-
+        map.svg.selectAll('path')
+            .data(data.features)
+            .enter().append('path')
+            .attr('class', 'state')
+            .attr('d', map.path);
     };
 
 }; // OhioMap
